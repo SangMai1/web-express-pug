a17993ef1a95a4dcb68bd3b5e47cc612e3f1c469
SangMai <vansang1532000@gmail.com>
2020-03-30 20:14:35 +0700

    middleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 var controller = require("../controllers/user.controller");
+var validate = require("../validate/user.validate");
 router.get("/", controller.index);
 
 router.get("/search", controller.search);
@@ -10,5 +11,5 @@ router.get("/create", controller.create);
 
 router.get("/:id", controller.get);
 
-router.post("/create", controller.postCreate);
+router.post("/create",validate.postCreate, controller.postCreate);
 module.exports = router;
